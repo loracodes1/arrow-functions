a6ab0d239420169a6fd909ad3201a7e11da1eef6
florence <loracodes3@gmail.com>
2024-10-07 12:48:47 +0300

    final commit

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function printBook(book){
     console.log(book)
 }
 
-const printBookArrow =book =>{
-    console.log(book)
-}
+const printBookArrow =book =>console.log(book)
 
 console.log("Eloquent JavaScript")
 console.log("You don't know JS")
