7183fd6080bfab42809292626fb51d36a6b5af81
florence <loracodes3@gmail.com>
2024-10-07 12:44:28 +0300

    console

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function printBook(book){
     console.log(book)
 }
 
-const printBookArrow
+const printBookArrow =book =>{
+    console.log(book)
+}
+
+console.log("Eloquent JavaScript")
+console.log("You don't know JS")
